cce8957518982e667556ae42de3d7beaa45a9b26
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2023-03-03 21:22:30 +0530

    created client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Layout from './components/Layout/Layout';
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>My App</h1>
+      </Layout>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h4 className='font-bold text'>All Rights Reserved @PramodReddy</h4>
+    </div>
+  )
+}
+
+export default Footer
