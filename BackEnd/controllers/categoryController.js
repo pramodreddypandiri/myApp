16d1daf725340b0659f7071dfc53e4ed066bdb5b
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2023-03-12 19:36:55 +0530

    cat controller done

--- BackEnd/controllers/categoryController.js
+++ BackEnd/controllers/categoryController.js
@@ -10,7 +10,7 @@ import Category from '../models/category.schema.js'
 * @route : /api/auth/v1/create-category
 * @description :  controller for creating
  new cat in DB
- * @Parametes name
+ * @Parametes title, userId
  @ returns User Object
  */
 export const createCategory = asyncHandler(async (req, res) => {
@@ -52,4 +52,86 @@ export const createCategory = asyncHandler(async (req, res) => {
     }
     
     
+})
+/*
+* @Delete category
+* @route : /api/auth/v1/delete-category
+* @description :  controller for deleting a cat in DB
+ * @Parametes title, userId
+ @ returns success,message 
+ */
+export const deleteCategory  = asyncHandler(async (req, res) => {
+   try{
+    const {id} = req.params
+    const {userId} = req.body
+    const deleteCat = await Category.findOneAndDelete({_id:id, userId :userId})
+    console.log(deleteCat);
+    res.status(200).json({
+        success: true,
+        message : "Delete cat successfully",
+
+    })
+   } catch(error){
+       console.log(error);
+       res.status(500).json({
+        success: false,
+        message : "Error in delete cat"
+       })
+
+   }
+})
+/*
+* @Update category
+* @route : /api/auth/v1/update-category
+* @description :  controller for deleting a cat in DB
+ * @Parametes title, userId
+ @ returns success,message 
+ */
+export const updateCategory = asyncHandler(async (req, res) => {
+    try {
+        const {title, userId} = req.body
+        const {id} = req.params
+        console.log(title, id);
+        const updatedCat = await Category.findOneAndUpdate({_id:id, userId :userId}, {title: title}, {new: true})
+        console.log("after update");
+        res.status(200).json({
+            success : true,
+            message: "Category updated successfully",
+            updatedCat
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            error,
+            message: "Error while updating category"
+        })
+    }
+    
+})
+/*
+* @Get all categories
+* @route : /api/auth/v1/categories
+* @description :  controller for getting all cat in DB
+ * @Parametes title, userId
+ @ returns success,message 
+ */
+export const getAllCategories = asyncHandler(async (req, res) => {
+    try {
+        const {userId} = req.body
+        const allCatOfUser = await Category.find({ userId: { $exists: true } })
+        console.log("after allCat");
+        res.status(200).json({
+            success: true,
+            message: "Fetched all cats",
+            allCatOfUser
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success : false,
+            message: "Error in getAllCatofUser"
+
+        })
+    }
 })
--- BackEnd/routes/categoryRoutes.js
+++ BackEnd/routes/categoryRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createCategory } from '../controllers/categoryController.js'
+import { createCategory, deleteCategory, getAllCategories, updateCategory } from '../controllers/categoryController.js'
 import {isLoggedIn} from '../middlewares/auth.middleware.js'
 
 
@@ -8,9 +8,10 @@ const router = express.Router()
 // create category
 router.post('/create-category', isLoggedIn, createCategory)
 // delete category
-
+router.post('/delete-category/:id', isLoggedIn, deleteCategory)
 
 // update category
-
-
+router.post('/update-category/:id', isLoggedIn, updateCategory)
+// get all cat
+router.get('/categories', isLoggedIn, getAllCategories)
 export default router
