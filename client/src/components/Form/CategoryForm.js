f6bb1e2ac6d025383da887e6322fca5973961e3c
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2023-03-18 18:14:39 +0530

    create category form

---
+++ client/src/components/Form/CategoryForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CategoryForm = ({handleSubmit, value, setValue}) => {
+  return (
+    <>
+      <form onSubmit={handleSubmit} className='flex my-4 w-full flex-row items-center '>
+        <div className=' w-full'>
+            <input type='text ' 
+            className='category-input w-full px-4 py-2'
+            placeholder='Enter new Category'
+            value={value
+            }
+            onChange={(e) => setValue(e.target.value)}/>
+        </div>
+        <button type = 'submit' className='bg-black ml-3 text-white rounded-lg py-2 px-4 '>     
+             Add
+        </button>
+      </form>
+    </>
+  )
+}
+
+export default CategoryForm
--- client/src/pages/User/CreateCategory.js
+++ client/src/pages/User/CreateCategory.js
@@ -5,11 +5,32 @@ import { useState, useEffect} from 'react'
 import { useAuth } from '../../context/auth'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
+import CategoryForm from '../../components/Form/CategoryForm'
 const CreateCategory = () => {
     const [categories, setCategories] = useState([]);
     const [auth, setAuth] = useAuth();
+    const [title, setTitle] = useState('')
+    const userId = auth.user._id
+    //handle category input submit
+    const handleSubmit = async () => {
+        try{
+            const {data} = await axios.post('/api/v1/category/create-category',{title, userId}) 
+            if(data?.success){
+                toast.success(`${data.title} is created`)
+                getAllCategories()
+            }
+            else{
+                toast.error(data.message)
+            }
+
+        } catch (error){
+            console.log(error);
+            toast.error("Error in cat input ")
+
+        }
+    }
     const getAllCategories = async () => {
-        const userId = auth.user._id
+        
         console.log(typeof(userId));
          console.log(userId)
         try{
@@ -37,25 +58,30 @@ const CreateCategory = () => {
             </div>
             <div className='ml-10 mt-10'>
                 <h4 className='text-2xl font-semibold'> Category</h4>
-                <div className='w-[500px]'>
-                    <tabel className="w-[500px] divide-y divide-gray-200">
+                <div>
+                    <CategoryForm handleSubmit={handleSubmit} value={title} setValue={setTitle}/>
+                </div>
+                <div className=''>
+                    <table className="min-w-[500px]">
                         <thead className="bg-black text-white">
                             <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Title</th>
-                            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Actions</th>
+                            <th className="px-6 py-3 text-xs font-medium uppercase tracking-wider">Actions</th>
                         </thead>
                         <tbody>
                            
-                           {categories.map((c) => (
+                           {categories?.map((c) => (
                                 <>
-                                   <tr>
-                                        <td className="px-6 py-4 whitespace-nowrap" key={c._id}>{c.title}</td>
-                                        <button className='bg-black px-4 py-2 rounded-lg text-white'>Edit</button>
+                                   <tr className=''>
+                                        <td className="px-6 py-4 text-left whitespace-nowrap" key={c._id}>{c.title}</td>
+                                        <td className='align-middle text-center'>
+                                        <button className='bg-black px-4 py-1 rounded-lg text-white'>Edit</button>
+                                        </td>
                                     </tr>
                                 </>
                                 ))}
                             
                         </tbody>
-                    </tabel>
+                    </table>
                 </div>
             </div>
         </div>
